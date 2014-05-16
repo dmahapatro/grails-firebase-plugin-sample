788ac495e41a9a218e3f3a3b8d9ac2d9c82bfac1
dmahapatro <dhiraj.mahapatro@gmail.com>
2014-05-16 00:28:43 -0400

    Initial Commit

---
+++ grails-app/controllers/com/example/firebase/FirebaseDemoController.groovy
@@ -0,0 +1,51 @@
+package com.example.firebase
+
+class FirebaseDemoController {
+    def firebaseRef
+
+    def index() {
+        //Firebase.defaultConfig = new Config(eventTarget: event)
+        //Firebase firebaseRef = new Firebase('https://torid-fire-7633.firebaseio.com')
+
+        firebaseRef.child("first").value = "X"
+        firebaseRef.child("last").value = "Doe"
+
+        firebaseRef.on('value'){ data, error ->
+            println "first name is ${data.value}"
+        }
+
+        // Writing data
+        firebaseRef.value  = [
+                address: [
+                        street: '123 Main Street',
+                        apt: 123,
+                        city: 'Columbus',
+                        zip: 43230,
+                        state: 'OH',
+                        country: 'USA'
+                ]
+        ]
+        firebaseRef.onChildAdded { data, previous, error ->
+            println "Add callback"
+            println data
+            println previous
+            println error
+        }
+
+        firebaseRef.on('childChanged'){ data, previous, error ->
+            println "Change callback"
+            println data
+            println previous
+            println error
+        }
+
+        firebaseRef.on('childRemoved'){ data, previous, error ->
+            println "Remove callback"
+            println data
+            println previous
+            println error
+        }
+
+        render "Firebase Ok"
+    }
+}
---
+++ test/unit/com/example/firebase/FirebaseDemoControllerSpec.groovy
@@ -0,0 +1,20 @@
+package com.example.firebase
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(FirebaseDemoController)
+class FirebaseDemoControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
